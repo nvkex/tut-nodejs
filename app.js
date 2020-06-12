0cf62d0ca0f8ac1dac75898b83ff8b8eab50eae8
Sumit Bhardwaj <40798467+nvkex@users.noreply.github.com>
2020-06-12 23:28:08 +0530

    added middleware for styles

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 var app = express();
 
 app.set('view-engine','ejs');
+app.use('/assets', express.static('assets'));
+
 
 app.get('/', function(req,res){
     res.render('index.ejs');
